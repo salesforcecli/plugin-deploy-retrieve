bebc4400d22c4955207ec26db8a9ee3b403e4e6b
Mike Donnalley <mdonnalley@salesforce.com>
2022-02-21 11:14:22 -0700

    chore: add tests

---
+++ test/commands/deploy/metadata.nut.ts
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2021, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+
+import * as path from 'path';
+import { SourceTestkit } from '@salesforce/source-testkit';
+
+describe('deploy metadata NUTs', () => {
+  let sourceTestkit: SourceTestkit;
+
+  before(async () => {
+    sourceTestkit = await SourceTestkit.create({
+      repository: 'https://github.com/trailheadapps/dreamhouse-lwc.git',
+      executable: path.join(process.cwd(), 'bin', 'dev'),
+      nut: __filename,
+    });
+  });
+
+  after(async () => {
+    await sourceTestkit?.clean();
+  });
+
+  describe('--source-dir flag', () => {
+    it('should deploy force-app', async () => {
+      await sourceTestkit.deploy({ args: '--source-dir force-app' });
+      await sourceTestkit.expect.filesToBeDeployed(['force-app/**/*'], ['force-app/test/**/*']);
+    });
+  });
+
+  describe('--metadata flag', () => {
+    it('should deploy ApexClass', async () => {
+      await sourceTestkit.modifyLocalGlobs(['force-app/main/default/classes/*.cls'], '// comment');
+      await sourceTestkit.deploy({ args: '--metadata ApexClass' });
+      await sourceTestkit.expect.filesToBeDeployed(['force-app/main/default/classes/*']);
+    });
+
+    it('should deploy named ApexClass', async () => {
+      await sourceTestkit.modifyLocalGlobs(
+        ['force-app/main/default/classes/GeocodingService.cls'],
+        '// another comment'
+      );
+      await sourceTestkit.deploy({ args: '--metadata ApexClass:GeocodingService' });
+      await sourceTestkit.expect.filesToBeDeployed(['force-app/main/default/classes/GeocodingService.cls']);
+    });
+
+    it('should deploy multiple metadata types', async () => {
+      await sourceTestkit.modifyLocalGlobs(['force-app/main/default/classes/*.cls'], '// comment');
+      await sourceTestkit.modifyLocalGlobs(['force-app/main/default/aura/**/*.cmp'], '<!-- comment -->');
+      await sourceTestkit.deploy({ args: '--metadata ApexClass AuraDefinitionBundle' });
+      await sourceTestkit.expect.filesToBeDeployed([
+        'force-app/main/default/classes/*',
+        'force-app/main/default/aura/**/*',
+      ]);
+    });
+  });
+
+  describe('--manifest flag', () => {
+    it('should deploy metadata specified in package.xml', async () => {
+      await sourceTestkit.modifyLocalGlobs(['force-app/main/default/classes/*.cls'], '// comment');
+      const xml = '<types><members>*</members><name>ApexClass</name></types>';
+      const packageXml = await sourceTestkit.createPackageXml(xml);
+
+      await sourceTestkit.deploy({ args: `--manifest ${packageXml}` });
+      await sourceTestkit.expect.filesToBeDeployed(['force-app/main/default/classes/*']);
+    });
+  });
+});
