a4f65daa9c4f6b9906110dcdde6c70667a6142a3
Willie Ruemmele <willieruemmele@gmail.com>
2023-05-11 10:46:26 -0600

    test: remove deprecated command from NUTs

--- test/nuts/tracking/forceIgnore.nut.ts
+++ test/nuts/tracking/forceIgnore.nut.ts
@@ -174,7 +174,7 @@ describe('forceignore changes', () => {
 
     it('sf will not retrieve a remote file added to the ignore AFTER it is being tracked', () => {
       // pull doesn't retrieve that change
-      const pullOutput = execCmd<RetrieveResultJson>('retrieve:metadata --json', {
+      const pullOutput = execCmd<RetrieveResultJson>('project:retrieve:start --json', {
         ensureExitCode: 0,
       }).jsonOutput?.result;
       expect(
