70157ad1bcfbd1f49e842af9397a921050ef9086
mshanemc <shane.mclaughlin@salesforce.com>
2023-06-12 09:14:17 -0500

    feat: more logging when progress disabled, separate test info, error details

--- src/utils/progressBar.ts
+++ src/utils/progressBar.ts
@@ -11,19 +11,23 @@ import { Messages } from '@salesforce/core';
 import { Progress } from '@salesforce/sf-plugins-core';
 
 Messages.importMessagesDirectory(__dirname);
-const mdTrasferMessages = Messages.loadMessages('@salesforce/plugin-deploy-retrieve', 'metadata.transfer');
+const mdTransferMessages = Messages.loadMessages('@salesforce/plugin-deploy-retrieve', 'metadata.transfer');
 
 export class DeployProgress extends Progress {
   private static OPTIONS = {
     title: 'Status',
-    format: '%s: {status} | {bar} | {value}/{total} Components',
+    format: `%s: {status} ${
+      env.getBoolean(EnvironmentVariable.SF_USE_PROGRESS_BAR, true) ? '| {bar} ' : ''
+    }| {value}/{total} Components (Errors:{errorCount}) {testInfo}`,
     barCompleteChar: '\u2588',
     barIncompleteChar: '\u2591',
     linewrap: true,
+    // only set the value if explicitly set to false, otherwise let the progress bar decide
+    noTTYOutput: env.getBoolean(EnvironmentVariable.SF_USE_PROGRESS_BAR, true) === false ? true : undefined,
   };
 
   public constructor(private deploy: MetadataApiDeploy, jsonEnabled = false) {
-    super(!jsonEnabled && env.getBoolean(EnvironmentVariable.SF_USE_PROGRESS_BAR, true));
+    super(!jsonEnabled);
   }
 
   public start(): void {
@@ -34,7 +38,7 @@ export class DeployProgress extends Progress {
     // any thing else should make one final update, then stop the progress bar
     this.deploy.onFinish((data) => {
       this.updateProgress(data.response);
-      this.finish({ status: mdTrasferMessages.getMessage(data.response.status) });
+      this.finish({ status: mdTransferMessages.getMessage(data.response.status) });
     });
 
     this.deploy.onCancel(() => this.stop());
@@ -47,18 +51,19 @@ export class DeployProgress extends Progress {
 
   private updateProgress(data: MetadataApiDeployStatus): void {
     // the numCompTot. isn't computed right away, wait to start until we know how many we have
+    const errorCount = data.numberComponentErrors ?? 0;
+    const testInfo = `| ${data.numberTestsCompleted ?? 0}/${data.numberTestsTotal ?? 0} Tests (Errors:${
+      data.numberTestErrors
+    })`;
     if (data.numberComponentsTotal) {
-      this.setTotal(data.numberComponentsTotal + data.numberTestsTotal);
-      this.update(data.numberComponentsDeployed + data.numberTestsCompleted, {
-        status: mdTrasferMessages.getMessage(data.status),
+      this.setTotal(data.numberComponentsTotal);
+      this.update(data.numberComponentsDeployed, {
+        errorCount,
+        status: mdTransferMessages.getMessage(data.status),
+        testInfo,
       });
     } else {
-      this.update(0, { status: mdTrasferMessages.getMessage(data.status) ?? 'Waiting' });
-    }
-
-    // the numTestsTot. isn't computed until validated as tests by the server, update the PB once we know
-    if (data.numberTestsTotal && data.numberComponentsTotal) {
-      this.setTotal(data.numberComponentsTotal + data.numberTestsTotal);
+      this.update(0, { errorCount, testInfo, status: mdTransferMessages.getMessage(data.status) ?? 'Waiting' });
     }
   }
 }
