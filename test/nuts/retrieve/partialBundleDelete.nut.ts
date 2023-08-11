995b6395ce4d5f811ebf25f77667986521fb068b
Steve Hetzel <shetzel@salesforce.com>
2023-08-11 17:29:30 -0600

    fix: update partial bundle delete tests for Dreamhouse source changes

--- test/nuts/retrieve/partialBundleDelete.nut.ts
+++ test/nuts/retrieve/partialBundleDelete.nut.ts
@@ -129,16 +129,16 @@ describe('Partial Bundle Delete Retrieves', () => {
     // component locally, then retrieve the component from the org which should
     // delete the CSS file added to match the component in the org.
     it('should replace and report local Aura content that was deleted for retrieve', () => {
-      const auraPropertyListMapPath = path.join(auraSrcDir, 'auraPropertyListMap');
+      const pageTemplatePath = path.join(auraSrcDir, 'pageTemplate_2_7_3');
 
-      // Add another CSS file to the auraPropertyListMap component. This file
+      // Add another CSS file to the pageTemplate_2_7_3 component. This file
       // should be deleted after a retrieve of the component from the org.
-      const testCssFile = path.join(auraPropertyListMapPath, 'testFile.css');
+      const testCssFile = path.join(pageTemplatePath, 'testFile.css');
       fs.writeFileSync(testCssFile, '.THIS header { display: none; }');
       expect(fs.existsSync(testCssFile)).to.be.true;
 
       const result = execCmd<RetrieveResultJson>(
-        `project retrieve start -d ${auraPropertyListMapPath} -o ${scratchOrgUsername} --json`,
+        `project retrieve start -d ${pageTemplatePath} -o ${scratchOrgUsername} --json`,
         { ensureExitCode: 0 }
       );
 
@@ -150,7 +150,7 @@ describe('Partial Bundle Delete Retrieves', () => {
       // find the deleted entry for testFile.css
       const deletedFileResponse = files.find((fr) => fr.state === 'Deleted');
       expect(deletedFileResponse).to.deep.equal({
-        fullName: 'auraPropertyListMap',
+        fullName: 'pageTemplate_2_7_3',
         type: 'AuraDefinitionBundle',
         state: 'Deleted',
         filePath: testCssFile,
