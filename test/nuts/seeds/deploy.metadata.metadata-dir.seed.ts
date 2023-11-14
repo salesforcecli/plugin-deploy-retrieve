e071ccdd0f100fa4aeae75559bc67adc086c2494
mshanemc <shane.mclaughlin@salesforce.com>
2023-11-14 08:01:48 -0600

    test: debug all the things

--- test/nuts/seeds/deploy.metadata.metadata-dir.seed.ts
+++ test/nuts/seeds/deploy.metadata.metadata-dir.seed.ts
@@ -4,6 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+/* eslint-disable no-console */
 
 import * as path from 'node:path';
 import { fileURLToPath } from 'node:url';
@@ -25,6 +26,7 @@ context('deploy metadata --metadata-dir NUTs [name: %REPO_NAME%]', () => {
       repository: REPO.gitUrl,
       nut: fileURLToPath(import.meta.url),
     });
+    console.log('before is done.  project/repo created');
   });
 
   after(async () => {
@@ -43,16 +45,23 @@ context('deploy metadata --metadata-dir NUTs [name: %REPO_NAME%]', () => {
         const paths = testCase.toDeploy.map((t) => path.normalize(t)).join(',');
         // This is using the force:source:convert command from plugin-source. Once we have an
         // sf equivalent, we should switch it to use that.
+        console.log('converting first paths: ', paths);
+
         await testkit.convert({ args: `--sourcepath ${paths} --outputdir out` });
+        console.log('converting done: ', paths);
+        console.log('deploying using mdapi');
 
         const deploy = await testkit.deploy<DeployResultJson>({ args: '--metadata-dir out' });
+        console.log('deploying complete');
         assert(deploy);
         testkit.expect.toHavePropertyAndValue(deploy.result as unknown as JsonMap, 'status', RequestStatus.Succeeded);
       });
     }
-
+    console.log('done with deploys that should succeed');
     it('should throw an error if the directory does not exist', async () => {
+      console.log('deploying a bad one');
       const deploy = await testkit.deploy({ args: '--metadata-dir DOES_NOT_EXIST', exitCode: 1 });
+      console.log('deploying done');
       assert(deploy);
       testkit.expect.errorToHaveName(deploy, 'InvalidFlagPathError');
     });
